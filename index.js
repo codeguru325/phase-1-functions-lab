a0d94f16cb9a39e92b1f994e6e3afc4fc422c8bc
samuel <sammykuria300@gmail.com>
2023-09-21 20:54:24 +0300

    Quick commit

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+ 
+function distanceFromHqInBlocks(pickupBlock) {
+        const hqBlock = 42; // Scuber headquarters is on 42nd Street
+        if (pickupBlock < hqBlock) {
+           let result = hqBlock - pickupBlock;
+           return result
+        } else {
+          let result =  pickupBlock - hqBlock;
+          return result
+        }
+      }
+      
+
+function distanceFromHqInFeet(pickupBlock){
+     const val = distanceFromHqInBlocks(pickupBlock)
+     const locCus = val * 264
+     return locCus
+   
+ }
+
+ function distanceTravelledInFeet(start, destination){
+
+    const disInBlock = Math.abs(start-destination)
+    const disInFeet = disInBlock * 264
+    return disInFeet
+ }
+
+function calculatesFarePrice(start, destination){
+
+   const fareFeet = distanceTravelledInFeet( start, destination)
+   if (fareFeet <= 400){
+      return 0
+   }else if(fareFeet > 400 && fareFeet <=2000) {
+      return (fareFeet - 400) * 2 / 100; 
+
+   }else if (fareFeet > 2000 && fareFeet <= 2500){
+      return 25
+   }else if (fareFeet > 2500){
+      return "cannot travel that far"
+   }
+}
